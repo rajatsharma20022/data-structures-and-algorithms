de8a1055e0db018c43b1c9e11d8bcf7ebf09a2ec
Rajat Sharma <rajatsharma20022@gmail.com>
2026-07-10 23:37:48 +0530

    Time: 0 ms (100%), Space: 43.8 MB (98.52%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -11,29 +11,29 @@
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         ListNode Rever_list=new ListNode(Integer.MIN_VALUE);
-        ListNode L1=list1;
-        ListNode L2=list2;
+        //ListNode L1=list1;
+        //ListNode L2=list2;
         ListNode Revhead=Rever_list;
-        while(L1!=null && L2 != null){
-            if(L1.val <= L2.val ) 
+        while(list1!=null && list2 != null){
+            if(list1.val <= list2.val ) 
             {
-                Revhead.next=new ListNode(L1.val);
-                 L1= L1.next;
+                Revhead.next=new ListNode(list1.val);
+                 list1= list1.next;
                  Revhead=Revhead.next;
             }
             else 
             {
-                Revhead.next=new ListNode(L2.val);
-               L2= L2.next;
+                Revhead.next=new ListNode(list2.val);
+               list2= list2.next;
                Revhead=Revhead.next;
             }
             
          }
-         if(L1 == null){
-            Revhead.next=L2;
+         if(list1 == null){
+            Revhead.next=list2;
          }
-         if(L2 == null){
-            Revhead.next=L1;
+         if(list2 == null){
+            Revhead.next=list1;
          }
          return Rever_list.next;
         
